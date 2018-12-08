ed34bb4c7e4af7ebf907723232cde38cbe274658
Giovinca <45704985+Giovinca@users.noreply.github.com>
2018-12-08 12:04:09 +0700

    Update wa.php

--- wa.php
+++ wa.php
@@ -1,5 +1,6 @@
 <?php
-echo "COPYRIGHT : SGB TEAM\n\n";
+echo "COPYRIGHT : GiovincaID\n\n";
+echo "Contact : ansorgiovinca@gmail.com
 echo "Nomor Target?\nInput : ";
 $nomer = trim(fgets(STDIN));
 if(strlen($nomer)==11){
